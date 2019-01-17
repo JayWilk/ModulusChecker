f321cc5c832af03106ea0caf0332c443aeb03440
Jay <wilkinson_929@hotmail.com>
2019-01-17 22:10:26 +0000

    Added the resource file to the core project

--- ModulusCheckerCore/Properties/Resources.Designer.cs
+++ ModulusCheckerCore/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ModulusChecker.Properties {
+namespace ModulusCheckerCore.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ModulusChecker.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ModulusChecker.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ModulusCheckerCore.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
