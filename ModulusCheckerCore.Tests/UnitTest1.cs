8521c9fb63d90042161fb990fa246c10222322dd
Jay <wilkinson_929@hotmail.com>
2019-01-17 22:06:16 +0000

    Initial checkin

---
+++ ModulusCheckerCore.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModulusCheckerCore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
